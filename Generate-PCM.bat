a7d296afe57b6283fcd9f95a0bd8b01c56257448
AkumaDark17 <akumaspartanmc@hotmail.com>
2023-05-18 18:00:32 +0200

    First Upload

---
+++ Generate-PCM.bat
@@ -0,0 +1,109 @@
+@echo off
+set "sm_folder=.\supermetroid_msu"
+set "z3_folder=.\alttp_msu"
+set "smz3_folder=.\smz3_msu"
+cd /d "%~dp0%sm_folder%"
+ren *-1.pcm sm_alttp_randomizer_msu1-101.pcm
+ren *-2.pcm sm_alttp_randomizer_msu1-102.pcm
+ren *-3.pcm sm_alttp_randomizer_msu1-103.pcm
+ren *-4.pcm sm_alttp_randomizer_msu1-104.pcm
+ren *-5.pcm sm_alttp_randomizer_msu1-105.pcm
+ren *-6.pcm sm_alttp_randomizer_msu1-106.pcm
+ren *-7.pcm sm_alttp_randomizer_msu1-107.pcm
+ren *-8.pcm sm_alttp_randomizer_msu1-108.pcm
+ren *-9.pcm sm_alttp_randomizer_msu1-109.pcm
+ren *-10.pcm sm_alttp_randomizer_msu1-110.pcm
+ren *-11.pcm sm_alttp_randomizer_msu1-111.pcm
+ren *-12.pcm sm_alttp_randomizer_msu1-112.pcm
+ren *-13.pcm sm_alttp_randomizer_msu1-113.pcm
+ren *-14.pcm sm_alttp_randomizer_msu1-114.pcm
+ren *-15.pcm sm_alttp_randomizer_msu1-115.pcm
+ren *-16.pcm sm_alttp_randomizer_msu1-116.pcm
+ren *-17.pcm sm_alttp_randomizer_msu1-117.pcm
+ren *-18.pcm sm_alttp_randomizer_msu1-118.pcm
+ren *-19.pcm sm_alttp_randomizer_msu1-119.pcm
+ren *-20.pcm sm_alttp_randomizer_msu1-120.pcm
+ren *-21.pcm sm_alttp_randomizer_msu1-121.pcm
+ren *-22.pcm sm_alttp_randomizer_msu1-122.pcm
+ren *-23.pcm sm_alttp_randomizer_msu1-123.pcm
+ren *-24.pcm sm_alttp_randomizer_msu1-124.pcm
+ren *-25.pcm sm_alttp_randomizer_msu1-125.pcm
+ren *-26.pcm sm_alttp_randomizer_msu1-126.pcm
+ren *-27.pcm sm_alttp_randomizer_msu1-127.pcm
+ren *-28.pcm sm_alttp_randomizer_msu1-128.pcm
+ren *-29.pcm sm_alttp_randomizer_msu1-129.pcm
+ren *-30.pcm sm_alttp_randomizer_msu1-130.pcm
+ren *-99.pcm sm_alttp_randomizer_msu1-99.pcm
+cd ..\..
+cd /d "%~dp0%z3_folder%"
+ren *-1.pcm sm_alttp_randomizer_msu1-1.pcm
+ren *-2.pcm sm_alttp_randomizer_msu1-2.pcm
+ren *-3.pcm sm_alttp_randomizer_msu1-3.pcm
+ren *-4.pcm sm_alttp_randomizer_msu1-4.pcm
+ren *-5.pcm sm_alttp_randomizer_msu1-5.pcm
+ren *-6.pcm sm_alttp_randomizer_msu1-6.pcm
+ren *-7.pcm sm_alttp_randomizer_msu1-7.pcm
+ren *-8.pcm sm_alttp_randomizer_msu1-8.pcm
+ren *-9.pcm sm_alttp_randomizer_msu1-9.pcm
+ren *-10.pcm sm_alttp_randomizer_msu1-10.pcm
+ren *-11.pcm sm_alttp_randomizer_msu1-11.pcm
+ren *-12.pcm sm_alttp_randomizer_msu1-12.pcm
+ren *-13.pcm sm_alttp_randomizer_msu1-13.pcm
+ren *-14.pcm sm_alttp_randomizer_msu1-14.pcm
+ren *-15.pcm sm_alttp_randomizer_msu1-15.pcm
+ren *-16.pcm sm_alttp_randomizer_msu1-16.pcm
+ren *-17.pcm sm_alttp_randomizer_msu1-17.pcm
+ren *-18.pcm sm_alttp_randomizer_msu1-18.pcm
+ren *-19.pcm sm_alttp_randomizer_msu1-19.pcm
+ren *-20.pcm sm_alttp_randomizer_msu1-20.pcm
+ren *-21.pcm sm_alttp_randomizer_msu1-21.pcm
+ren *-22.pcm sm_alttp_randomizer_msu1-22.pcm
+ren *-23.pcm sm_alttp_randomizer_msu1-23.pcm
+ren *-24.pcm sm_alttp_randomizer_msu1-24.pcm
+ren *-25.pcm sm_alttp_randomizer_msu1-25.pcm
+ren *-26.pcm sm_alttp_randomizer_msu1-26.pcm
+ren *-27.pcm sm_alttp_randomizer_msu1-27.pcm
+ren *-28.pcm sm_alttp_randomizer_msu1-28.pcm
+ren *-29.pcm sm_alttp_randomizer_msu1-29.pcm
+ren *-30.pcm sm_alttp_randomizer_msu1-30.pcm
+ren *-31.pcm sm_alttp_randomizer_msu1-31.pcm
+ren *-32.pcm sm_alttp_randomizer_msu1-32.pcm
+ren *-33.pcm sm_alttp_randomizer_msu1-33.pcm
+ren *-34.pcm sm_alttp_randomizer_msu1-34.pcm
+ren *-35.pcm sm_alttp_randomizer_msu1-35.pcm
+ren *-36.pcm sm_alttp_randomizer_msu1-36.pcm
+ren *-37.pcm sm_alttp_randomizer_msu1-37.pcm
+ren *-38.pcm sm_alttp_randomizer_msu1-38.pcm
+ren *-39.pcm sm_alttp_randomizer_msu1-39.pcm
+ren *-40.pcm sm_alttp_randomizer_msu1-40.pcm
+ren *-41.pcm sm_alttp_randomizer_msu1-41.pcm
+ren *-42.pcm sm_alttp_randomizer_msu1-42.pcm
+ren *-43.pcm sm_alttp_randomizer_msu1-43.pcm
+ren *-44.pcm sm_alttp_randomizer_msu1-44.pcm
+ren *-45.pcm sm_alttp_randomizer_msu1-45.pcm
+ren *-46.pcm sm_alttp_randomizer_msu1-46.pcm
+ren *-47.pcm sm_alttp_randomizer_msu1-47.pcm
+ren *-48.pcm sm_alttp_randomizer_msu1-48.pcm
+ren *-49.pcm sm_alttp_randomizer_msu1-49.pcm
+ren *-50.pcm sm_alttp_randomizer_msu1-50.pcm
+ren *-51.pcm sm_alttp_randomizer_msu1-51.pcm
+ren *-52.pcm sm_alttp_randomizer_msu1-52.pcm
+ren *-53.pcm sm_alttp_randomizer_msu1-53.pcm
+ren *-54.pcm sm_alttp_randomizer_msu1-54.pcm
+ren *-55.pcm sm_alttp_randomizer_msu1-55.pcm
+ren *-56.pcm sm_alttp_randomizer_msu1-56.pcm
+ren *-57.pcm sm_alttp_randomizer_msu1-57.pcm
+ren *-58.pcm sm_alttp_randomizer_msu1-58.pcm
+ren *-59.pcm sm_alttp_randomizer_msu1-59.pcm
+ren *-60.pcm sm_alttp_randomizer_msu1-60.pcm
+ren *-61.pcm sm_alttp_randomizer_msu1-61.pcm
+cd ..\..
+cd /d "%~dp0%sm_folder%"
+for %%I in (*.pcm) do (
+    copy "%%I" "%~dp0%smz3_folder%\"
+)
+cd ..\..
+cd /d "%~dp0%z3_folder%"
+for %%I in (*.pcm) do (
+    copy "%%I" "%~dp0%smz3_folder%\"
+)
---
+++ Readme.txt
@@ -0,0 +1,9 @@
+1) Place each PCM Sets in the corresponding directories (by default: "alttp_msu" and "supermetroid_msu"), these must be in the same directory as "Generate-PCM.bat" File.
+
+2) Double click on "Generate-PCM.bat" File.
+
+3) Rename a ROM File to "sm_alttp_randomizer_msu1.sfc" and place it in "smz3_msu" directory folder.
+
+4) Make sure that "sm_alttp_randomizer_msu1.msu" exist in the "smz3_msu" directory folder (if missing, create an empty file with that name and extension).
+
+Original program from ABOhiccups: https://github.com/ABOhiccups/MSU1-PCM-Rename-Generator
